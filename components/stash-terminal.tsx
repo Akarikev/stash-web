d5e7a245095e4e3dbec50330537e3e9445c5672d
Akarikev <princeelorm17@gmail.com>
2024-03-24 03:46:48 +0000

    mod: added cal

--- components/stash-terminal.tsx
+++ components/stash-terminal.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import Cal from "@calcom/embed-react";
 
 function StashTerminal() {
   return (
@@ -15,7 +17,31 @@ function StashTerminal() {
         </div>
 
         {/* Video embed */}
-        <div>dif</div>
+        <div>
+          <iframe
+            src="https://www.youtube-nocookie.com/embed/ihE8OYTdklg?si=KQ4X7-RonQgXhodH&amp;controls=0"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            className="lg:w-[921px] lg:h-[509px] rounded-lg mt-3 w-[380px] h-[200px]"
+          ></iframe>
+        </div>
+
+        {/* Cal embed */}
+        <div className="mt-6">
+          <h1 className="lg:text-6xl mt-3 text-black font-extrabold text-center text-2xl tracking-tight">
+            Schedule a Demo
+          </h1>
+
+          <div className="border-4 border-black rounded-md">
+            {/* <Cal
+              calLink="rick/get-rick-rolled"
+              className="w-[380px] lg:w-full "
+            ></Cal> */}
+            [cal edmbed heres]
+          </div>
+        </div>
       </div>
     </div>
   );
